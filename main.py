66d5672b5907cebc509cf6564ef31f877245f8ab
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-08 10:49:39 +0800

    update: starting the page

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import random as rd
+from datetime import datetime as dt
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    rand_num = rd.randint(1,10)
+    dt_year = dt.year
+    return render_template('index.html', num=rand_num, year=dt_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
